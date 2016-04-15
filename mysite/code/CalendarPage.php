3064e29684d67453c440d406681b8464fff4ce07
Heath Dunlop <heath@Developer-07.local>
2016-04-15 16:43:47 +1200

    init

---
+++ mysite/code/CalendarPage.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: heath
+ * Date: 15/04/16
+ * Time: 4:42 PM
+ */
+class CalendarPage extends Page {
+
+}
+
+class CalendarPage_Controller extends Page_Controller {
+    
+}
