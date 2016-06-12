11a1497476fd99b5be958278fc6f57af3fd7c680
Dunatron <heath.dunlop.hd@gmail.com>
2016-06-12 23:37:31 +1200

    added no scrollbar script to calendar build

--- mysite/code/CalendarPage.php
+++ mysite/code/CalendarPage.php
@@ -684,6 +684,28 @@ class CalendarPage_Controller extends Page_Controller
         /* end calendar */
         /* all done, return result */
 
+        $calendar .= '<script>
+        (function(c){var a=["DOMMouseScroll","mousewheel"];c.event.special.mousewheel={setup:function(){if(this.addEventListener){for(var d=a.length;d;){this.addEventListener(a[--d],b,false)}}else{this.onmousewheel=b}},teardown:function(){if(this.removeEventListener){for(var d=a.length;d;){this.removeEventListener(a[--d],b,false)}}else{this.onmousewheel=null}}};c.fn.extend({mousewheel:function(d){return d?this.bind("mousewheel",d):this.trigger("mousewheel")},unmousewheel:function(d){return this.unbind("mousewheel",d)}});function b(f){var d=[].slice.call(arguments,1),g=0,e=true;f=c.event.fix(f||window.event);f.type="mousewheel";if(f.wheelDelta){g=f.wheelDelta/120}if(f.detail){g=-f.detail/3}d.unshift(f,g);return c.event.handle.apply(this,d)}})(jQuery);
+
+/*
+ Configure the below code to whatever div you want to scroll
+ */
+$(".day-square").bind("mousewheel",function(ev, delta) {
+    var scrollTop = $(this).scrollTop();
+    $(this).scrollTop(scrollTop-Math.round(delta * 20));
+});
+
+$(".modal-body").bind("mousewheel",function(ev, delta) {
+    var scrollTop = $(this).scrollTop();
+    $(this).scrollTop(scrollTop-Math.round(delta * 20));
+});
+
+$(".calendarpage").bind("mousewheel",function(ev, delta) {
+    var scrollTop = $(this).scrollTop();
+    $(this).scrollTop(scrollTop-Math.round(delta * 20));
+});
+        </script>';
+
         return $calendar;
     }
 
