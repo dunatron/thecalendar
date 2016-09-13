aa2904023e87931a673ff0b744595a54ea60ad34
Dunatron <heath.dunlop.hd@gmail.com>
2016-09-14 08:36:50 +1200

    ff

--- themes/happ-dcc/js/select2/custom-select2.js
+++ themes/happ-dcc/js/select2/custom-select2.js
@@ -1,6 +1,50 @@
 /**
  * Created by Heath on 12/09/16.
  */
-$('#tag-drop-down').select2({
+var tagsDropdown = $('#tag-drop-down').select2({
     tags:true
 });
+
+/**
+ * When A tag(filter) has been selected do stuff
+ * 1. get children of selection (select2-selection__rendered)
+ */
+tagsDropdown.on("select2:select", function (e) {
+   console.log("A tag was selected");
+
+    var filterTAGS = [];
+    var filterValues = [];
+
+    //select2-selection__rendered
+    var tagsHolder = $('.select2-selection__rendered');
+    var realatedList = $('.RealTagsHolder');
+
+    function getFilterTags() {
+        var tags = tagsHolder.children().each(function () {
+            var tagTitle = $(this).attr("title");
+            filterTAGS.push(tagTitle);
+        });
+    }
+
+    getFilterTags();
+
+    var relatedTagIDS = realatedList.children().each(function () {
+        $tagTitle = $(this).attr("data-title");
+        $tagID = $(this).attr("data-tag");
+        // if(jQuery.inArray($tagTitle, filterTAGS) ==1) {
+        //     console.log('ITS VERY TRUE');
+        // }
+        if($.inArray(filterTAGS, $tagTitle)) {
+            filterValues.push($tagID);
+            console.log("SURELY");
+        }
+
+        console.log($tagTitle);
+    });
+
+    console.log(filterTAGS);
+    console.log(filterValues);
+
+});
+
+//select2-selection__choice
--- themes/happ-dcc/templates/Includes/EventFilter.ss
+++ themes/happ-dcc/templates/Includes/EventFilter.ss
@@ -4,10 +4,10 @@
            <div class="filter-event-holder">
                <div class="form-group">
                    <label for="event-filter-dropdown" class="dropdown-label"></label>
-                   <select class="select-style" id="tag-drop-down" multiple="" tabindex="-1" aria-hidden="true">
+                   <select class="select-style RealTagsHolder" id="tag-drop-down" multiple="" tabindex="-1" aria-hidden="true">
                        <option value="placeholder" data-tag="none" data-title="placeholder"><a href="#">-- Select Tag --</a></option>
                        <% loop $CalendarTags %>
-                           <option value="$ID" data-tag="$ID" data-title="$title"><a href="#">$title</a></option>
+                           <option class="realTags" value="$ID" data-tag="$ID" data-title="$title"><a href="#">$title</a></option>
                        <% end_loop %>
                    </select>
                    <%--<a class="search-section-submit" id="viewAllEvents">View All</a>--%>
