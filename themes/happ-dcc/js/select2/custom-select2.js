df1b541b0fd3867c6e899df14666a2c1293e9ae6
Dunatron <heath.dunlop.hd@gmail.com>
2016-09-17 01:25:52 +1200

    I stay blazin like im brandon roy

--- themes/happ-dcc/js/select2/custom-select2.js
+++ themes/happ-dcc/js/select2/custom-select2.js
@@ -8,6 +8,9 @@ var tagsDropdown = $('#tag-drop-down').select2({
 /**
  * When A tag(filter) has been selected do stuff
  * 1. get children of selection (select2-selection__rendered)
+ *
+ *  selecting a TAG
+ *
  */
 
 
@@ -33,20 +36,120 @@ tagsDropdown.on("select2:select", function (e) {
     var relatedTagIDS = realatedList.children().each(function () {
         var tagTitle = $(this).attr("data-title");
         var tagID = $(this).attr("data-tag");
-
         if(jQuery.inArray(tagTitle, filterTAGS)!='-1') {
             console.log("IN THE ARRAY");
             filterValues.push(tagID);
-
         } else {
             console.log("NOT IN ARRAY");
         }
-
     });
 
-    console.log('filter tags' + filterTAGS);
-    console.log('filter values' + filterValues);
+
+    function filterEvents(){
+
+        console.log("TIME TO FILTER EVENTS");
+        console.log("TIME TO FILTER EVENTS AND VALUES " + filterValues);
+
+        var events = $('.event-btn');
+        jQuery.each(events, function () {
+            console.log("COUNT THE EVENTS");
+            var eventTags = $(this).attr("data-tag");
+            console.log("COUNT THE EVENTS and the tag " + eventTags);
+            // compare if event tag ID is present in the filterTAGS list
+            if(jQuery.inArray(eventTags, filterValues)!='-1') {
+                console.log("SHOW EVENTS");
+                // append show event OR AND remove hide-event class
+                $(this).removeClass('hide-event');
+
+            } else {
+                console.log("NOT IN ARRAY");
+                $(this).addClass('hide-event');
+            }
+        });
+    }
+
+    filterEvents();
 
 });
+
+
+/**
+ *
+ *  un-selecting a TAG
+ */
+
+// when this is clicked '.select2-selection__choice__remove'
+// check what tags are still up
+
+tagsDropdown.on("select2:unselect", function (e) {
+    console.log("A tag was selected");
+
+    var filterTAGS = []; // All tags from the calendar
+    var filterValues = []; // selected filter tags
+
+    //select2-selection__rendered
+    var tagsHolder = $('.select2-selection__rendered');
+    var realatedList = $('.RealTagsHolder');
+
+    function getFilterTags() {
+        var tags = tagsHolder.children().each(function () {
+            var tagTitle = $(this).attr("title");
+            filterTAGS.push(tagTitle);
+        });
+    }
+
+    getFilterTags();
+
+    var relatedTagIDS = realatedList.children().each(function () {
+        var tagTitle = $(this).attr("data-title");
+        var tagID = $(this).attr("data-tag");
+        if(jQuery.inArray(tagTitle, filterTAGS)!='-1') {
+            console.log("IN THE ARRAY");
+            filterValues.push(tagID);
+        } else {
+            console.log("NOT IN ARRAY");
+        }
+    });
+
+
+    function filterEvents(){
+
+        console.log("TIME TO FILTER EVENTS");
+        console.log("TIME TO FILTER EVENTS AND VALUES " + filterValues);
+
+        var events = $('.event-btn');
+        jQuery.each(events, function () {
+            console.log("COUNT THE EVENTS");
+            var eventTags = $(this).attr("data-tag");
+            console.log("COUNT THE EVENTS and the tag " + eventTags);
+            // compare if event tag ID is present in the filterTAGS list
+            if(jQuery.inArray(eventTags, filterValues)!='-1') {
+                console.log("SHOW EVENTS");
+                // append show event OR AND remove hide-event class
+                $(this).removeClass('hide-event');
+
+            } else {
+                console.log("NOT IN ARRAY");
+                $(this).addClass('hide-event');
+            }
+        });
+    }
+
+    filterEvents();
+
+    if (filterValues.length == 0){
+        var events = $('.event-btn');
+        jQuery.each(events, function () {
+            events.removeClass('hide-event');
+            //alert(events);
+        });
+        // alert("time to show all the events again cgump");
+    }
+
+    console.log('array size' + filterValues.length);
+
+
+});
+
 
 //select2-selection__choice
