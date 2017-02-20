afa80b9cc4d0f99dac9f3e61b23c4071eeeba200
Heath Dunlop <heath.dunlop@samdog.nz>
2017-02-20 16:20:16 +1300

    As close as I can get as Lee Sin

--- themes/happ/js/filter/filter.js
+++ themes/happ/js/filter/filter.js
@@ -2,7 +2,9 @@
  * Created by admin on 17/02/17.
  */
 var FilterTagsHolder = $('.RealTagsHolder'),
-    FilterModal = $('#FilterModal');
+    FilterModal = $('#FilterModal'),
+    currentTagArray = [],
+    event = $('.event-btn');
 
 $(FilterModal).modal({
     backdrop: false,
@@ -12,3 +14,48 @@ $(FilterModal).modal({
 $(FilterTagsHolder).select2({
     placeholder: "Filter..."
 });
+
+$(FilterTagsHolder).on('select2:select', function(){
+    currentTags();
+    console.log(currentTagArray);
+    applyFilter();
+});
+
+$(FilterTagsHolder).on('select2:unselect', function(){
+    currentTags();
+    console.log(currentTagArray);
+});
+
+function currentTags(){
+    var TagData = $(FilterTagsHolder).select2('data');
+    currentTagArray = [];
+    $.each(TagData, function( key, value ){
+        currentTagArray.push(value.text);
+    });
+}
+
+function applyFilter(){
+    // $.each(event, function(index, value){
+    //     console.log('div' + index + ':' + $(this).attr('eid'));
+    // });
+    var myCount=0;
+    $('.event-btn').each(function(){
+       //console.log($(this).attr('data-tag'));
+        var eventTags = $(this).attr('data-tag');
+        // seperate eventTags string by delimeter | then do array loop
+        if(!$.inArray(eventTags, currentTagArray)){
+            console.log('WE have found ONE');
+            $(this).addClass('hide-event');
+        } else {
+            $(this).removeClass('hide-event');
+        }
+        //console.log(eventTags + myCount++);
+
+    });
+
+}
+
+
+// var array = string.split('|');
+
+
