1cc575eaa9adf03f6261938a4679bdf62d37c3df
Heath Dunlop <heath.dunlop@samdog.nz>
2017-04-11 18:39:51 +1200

    ff

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -257,7 +257,7 @@ class Page_Controller extends ContentController {
             'KeyWord'   =>  $data['Search'],
         ));
 
-        $ResultsList->sort('ABS(UNIX_TIMESTAMP() - UNIX_TIMESTAMP(EventDate))');
+        $ResultsList->sort('EventDate', 'ABS(UNIX_TIMESTAMP() - UNIX_TIMESTAMP(EventDate))');
 
         echo $data->renderWith('Search_Results');
         //return $this->owner->customise($data)->renderWith('Search_Results');
