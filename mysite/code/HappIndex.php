227c9d97770b60a89dfa5e4c555c3220e147b49c
Heath Dunlop <heath.dunlop@samdog.nz>
2017-04-11 17:31:46 +1200

    ff

--- mysite/code/HappIndex.php
+++ mysite/code/HappIndex.php
@@ -6,6 +6,7 @@ class HappIndex extends SolrIndex {
         //https://github.com/silverstripe/silverstripe-fulltextsearch/blob/master/docs/en/Solr.md
         $this->addClass('Event');
         $this->addAllFulltextFields();
+        $this->addFulltextField('_versionedstage');
         $this->addStoredField('Content');
         $this->addStoredField('Title');
         // $this->addFulltextField('Title');
