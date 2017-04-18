510d3c307742a7ed65e2915c759a679d0b5f39d9
Dunatron <heath.dunlop.hd@gmail.com>
2017-04-18 22:55:30 +1200

    ff

--- mysite/code/Tasks/PullEventFindaEventsAdvanced.php
+++ mysite/code/Tasks/PullEventFindaEventsAdvanced.php
@@ -87,7 +87,10 @@ class PullEventFindaEventsAdvanced extends BuildTask
             // Finish Time
             $newEvent->FinishTime = $event->datetime_end;
             $newEvent->IsFree = $event->is_free;
-            $newEvent->BookingWebsite = $event->booking_web_site->url;
+            if($event->booking_web_site->url)
+            {
+                $newEvent->BookingWebsite = $event->booking_web_site->url;
+            }
             // Try store website if we have one. (just get the first site)
             if (!empty($event->web_sites->web_sites)) {
                 $newEvent->TicketWebsite = $event->web_sites->web_sites[0]->url;
